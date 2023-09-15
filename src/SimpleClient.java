45e70dc84e252a4bdb48a0787d4f8465e522ee1b
Finnegan Z. Chamberlain <fc84@st-andrews.ac.uk>
2023-09-15 11:41:01 +0100

    Updated Server and client to communicate and for communication to take place in seperate method

--- src/SimpleClient.java
+++ src/SimpleClient.java
@@ -8,6 +8,7 @@ public class SimpleClient {
 
     private String IPAddress = "";
     private String portnumber = "";
+    static int userResponseTime = 5000;
 
     public static void main(String[] args) {
         try {
@@ -23,8 +24,14 @@ public class SimpleClient {
             Socket connection = startClient(args[0], args[1]);
             OutputStream out = connection.getOutputStream();
             InputStream in = connection.getInputStream();
+            Thread.sleep(userResponseTime);
+
+            runProtocol(in, out, connection);
+
         } catch (IOException e) {
             System.err.println("IO Exception: " + e.getMessage());
+        } catch (InterruptedException e) {
+            System.err.println("Interrupted Exception: " + e.getMessage());
         }
     }
 
@@ -42,5 +49,34 @@ public class SimpleClient {
             System.err.println("IO Exception: " + e.getMessage());
         }
         return connection;
+    }
+
+    public static void runProtocol(InputStream in, OutputStream out, Socket connection) {
+        try {
+            byte[] buffer = new byte[80];
+            int b = 0;
+            if (System.in.available() > 0) {
+                b = System.in.read(buffer);
+            }
+
+            if (b > 0) {
+                out.write(buffer, 0, b);
+            }
+
+            Thread.sleep(userResponseTime);
+            buffer = new byte[80];
+            b = in.read(buffer);
+
+//            if (b > 0) {
+//                String s = new String(buffer);
+//                System.out.println("-> " + s);
+//            }
+
+            connection.close();
+        } catch (InterruptedException e) {
+            System.err.println("Interrupted Exception: " + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("IO Exception: " + e.getMessage());
+        }
     }
 }
--- src/SimpleServer.java
+++ src/SimpleServer.java
@@ -29,7 +29,7 @@ public class SimpleServer {
 
             System.out.println("New connection ... " + connection.getInetAddress().getHostName() + ":" + connection.getPort());
 
-            byte[] buffer = new byte[80];
+            runProtocol(in, out, connection);
 
         } catch (IOException e) {
             System.err.println("IO Exception: " + e.getMessage());
@@ -40,6 +40,29 @@ public class SimpleServer {
         try {
             server = new ServerSocket(port);
             System.out.println("--> Starting Server " + server.toString() + " <--");
+        } catch (IOException e) {
+            System.err.println("IO Exception: " + e.getMessage());
+        }
+    }
+
+    public static void runProtocol(InputStream in, OutputStream out, Socket connection) {
+        try {
+            byte[] buffer = new byte[80];
+            int b = 0;
+            while (b < 1) {
+                Thread.sleep(100);
+                buffer = new byte[80];
+                b = in.read(buffer);
+            }
+
+            if (b > 0) {
+                String s = new String(buffer);
+                System.out.println("-> " + s);
+                out.write(buffer, 0, b);
+                connection.close();
+            }
+        } catch (InterruptedException e) {
+            System.err.println("Interrupted Exception: " + e.getMessage());
         } catch (IOException e) {
             System.err.println("IO Exception: " + e.getMessage());
         }
