58dd0af3232772f2036c8c4308fa83a028600ed4
Finnegan Z. Chamberlain <fc84@st-andrews.ac.uk>
2023-09-25 21:08:39 +0100

    Commented both the server and client code

--- src/SimpleClient.java
+++ src/SimpleClient.java
@@ -5,13 +5,26 @@ import java.util.Scanner;
 
 public class SimpleClient {
 
-    private String IPAddress = "";
-    private String portnumber = "";
-    static int socketTimeout = 5000;
+    /**
+     * The private integer, socketTimeout, exists purely as a final value for the client to define
+     * the socket timeout. This was implemented to prevent errors in communication and latency.
+     * The protocol array stores the valid arguments for the specification defined communication
+     * protocol. This array also stores two extra values, "VALID". These values are to ensure that
+     * the final message is valid before cutting the connection.
+     */
+    static final int socketTimeout = 5000;
     private static String[] protocol = new String[]{"HELLO ADVISER", "HELLO ADVISEE",
             "ADVISE ME ON TO CS2003", "YOU ARE ADVISED ON TO CS2003", "THANK YOU",
             "YOU'RE WELCOME", "VALID", "VALID"};
 
+    /**
+     * The main method has two actions. It first grabs the command line argument and ensures that there
+     * are the appropriate items, IP address and port number. An array index out of bounds exception is
+     * thrown if these arguments are not provided and a helpful hint message is supplied to the user.
+     * The second action of the main method is to start the client connection using a socket with the user
+     * designated port and IP address.
+     * @param args the command line arguments - IP Address and Port number
+     */
     public static void main(String[] args) {
         try {
             if (args.length != 2) {
@@ -29,10 +42,19 @@ public class SimpleClient {
             runProtocol(connection);
 
         } catch (IOException e) {
-            System.err.println("IO Exception: " + e.getMessage());
+            System.out.println("Connection refused");
+            System.exit(1);
         }
     }
 
+    /**
+     * Start client takes the user defined IP address and port number to create a socket
+     * connection with the server. An appropriate message is printed if the connection
+     * is made and also if it is refused.
+     * @param IPAddress the user defined IP address
+     * @param portnumber the user defined port number
+     * @return Socket connection for the client to communicate with server
+     */
     public static Socket startClient(String IPAddress, String portnumber) {
         Socket connection = null;
         try {
@@ -50,6 +72,22 @@ public class SimpleClient {
         return connection;
     }
 
+    /**
+     * This method takes a socket connection parameter and begins communication with the server.
+     * A try/catch loop is implemented to catch and errors that might occur in the protocol. A
+     * buffered reader and print stream are created to receive and write data from and to the
+     * server. Two strings are created for the client's and server's response. ProtoNumber is
+     * an integer created to keep track of which protocol argument the message is currently on.
+     * A while loop that executes until protoNumber reach max length enables the client to grab
+     * the valid next message. A conditional statement checks that the server response is equal
+     * to protoNumber minus one which would be the appropriate previous message. If this is the
+     * first message, then it is also passed. There is one final check which exists to exit and
+     * stop printing to the terminal if the message equals one of the last two protocol messages,
+     * "VALID". For each iteration, the client response is printed and sent to the server. Should
+     * the received message not be valid the previous message in the protocol will be printed.
+     * Finally, the connection is closed.
+     * @param connection a socket object with a connection to the server
+     */
     public static void runProtocol(Socket connection) {
         try {
             BufferedReader serverInput = new BufferedReader(new InputStreamReader(connection.getInputStream()));
--- src/SimpleServer.java
+++ src/SimpleServer.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 
 public class SimpleServer {
 
+    /**
+     * The private integer, port, exists to enable easy access in the class. This is the same for
+     * the ServerSocket object, server. The protocol array stores the valid arguments for the
+     * specification defined communication protocol. This array also stores two extra values, "VALID".
+     * These values are to ensure that the final message is valid before cutting the connection.
+     */
     private static int port;
     private static ServerSocket server;
 
@@ -12,6 +18,14 @@ public class SimpleServer {
             "ADVISE ME ON TO CS2003", "YOU ARE ADVISED ON TO CS2003", "THANK YOU",
             "YOU'RE WELCOM", "VALID", "VALID"};
 
+    /**
+     * The main method has two actions. It first grabs the command line argument and ensures that there
+     * is only the appropriate item, port number. An array index out of bounds exception is
+     * thrown if the arguments are not provided and a helpful hint message is supplied to the user.
+     * The second action of the main method is to start the client connection using a socket with the user
+     * designated port.
+     * @param args the command line arguments - port number
+     */
     public static void main(String[] args) {
         try {
             if (args.length != 1) {
@@ -33,19 +47,42 @@ public class SimpleServer {
             runProtocol(connection);
 
         } catch (IOException e) {
-            System.err.println("IO Exception: " + e.getMessage());
+            System.out.println("Connection refused");
+            System.exit(1);
         }
     }
 
+    /**
+     * Start server takes the user defined port number to create a server socket
+     * connection point. An appropriate message is printed if the connection
+     * is made and also if it is refused.
+     */
     public static void startServer() {
         try {
             server = new ServerSocket(port);
-            System.out.println("--> Starting Server " + server.toString() + " <--");
+            System.out.println("--> Starting Server " + server + " <--");
         } catch (IOException e) {
-            System.err.println("IO Exception: " + e.getMessage());
+            System.out.println("Connection refused");
+            System.exit(1);
         }
     }
 
+    /**
+     * This method takes a socket connection parameter and begins communication.
+     * A try/catch loop is implemented to catch and errors that might occur in the protocol. A
+     * buffered reader and print stream are created to receive and write data from and to the
+     * server. Two strings are created for the client's and server's response. ProtoNumber is
+     * an integer created to keep track of which protocol argument the message is currently on.
+     * A while loop that executes until protoNumber reach max length enables the server to grab
+     * the next valid message. First a condition statement ensures the last two "VALID" messages
+     * are not print. Another conditional statement checks that the server response is equal
+     * to protoNumber minus one which would be the appropriate previous message. There is one
+     * final check which exists to exit and stop printing to the terminal if the message equals
+     * one of the last two protocol messages, "VALID". For each iteration, the server's response
+     * is printed and sent to the client. Should the received message not be valid the previous
+     * message in the protocol will be printed. Finally, the connection is closed.
+     * @param connection a sever socket which enables client connections
+     */
     public static void runProtocol(Socket connection) {
         try {
             BufferedReader clientInput = new BufferedReader(new InputStreamReader(connection.getInputStream()));
